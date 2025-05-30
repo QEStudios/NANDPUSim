3745d8cff7c3a0831a1d0810f34a36dae3d528ec
SKM GEEK <qestudios17@gmail.com>
2025-05-30 13:15:55 +1000

    Refactor

--- nandpu.go
+++ nandpu.go
@@ -28,11 +28,11 @@ var Logger *log.Logger
 
 func NewNANDPU() *NANDPU {
 	Logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime)
-	nandpu := NANDPU{}
-	nandpu.Mem.AddRegion(0x0000, 0x7FFF, NewRAM(0x0000, 0x8000)) // 32K RAM (CY62256N)
-	nandpu.Mem.AddRegion(0x8000, 0xFFFF, NewROM(0x8000, 0x8000)) // 32K ROM (AT28C256)
+	c := NANDPU{}
+	c.Mem.AddRegion(0x0000, 0x7FFF, NewRAM(0x0000, 0x8000)) // 32K RAM (CY62256N)
+	c.Mem.AddRegion(0x8000, 0xFFFF, NewROM(0x8000, 0x8000)) // 32K ROM (AT28C256)
 	Logger.Println("Initialised NANDPU")
-	return &nandpu
+	return &c
 }
 
 func (c *NANDPU) Step() { // TODO
